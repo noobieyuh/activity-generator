d78cef261b88819fa791e8461165e07b1986ba91
noobie <156363695+noobieyuh@users.noreply.github.com>
2024-12-06 18:49:15 +0000

    commiting current progress!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function activityGenerator()
 {
     var activity = "";
     // generate 200 different responses!
-    var activitySelect = (Math.floor(Math.random() * 2) + 1);
+    var activitySelect = (Math.floor(Math.random() * 100) + 1);
     // for debug
     console.log(activitySelect);
 
@@ -13,6 +13,282 @@ function activityGenerator()
     if(activitySelect == 2)
     {
         activity = "Go look for geocaches!";
+    }
+    if(activitySelect == 3)
+    {
+        activity = "Count how many trees you see outside!";
+    }
+    if(activitySelect == 4)
+    {
+        activity = "Listen to new music genres!";
+    }
+    if(activitySelect == 5)
+    {
+        activity = "Learn a new language!";
+    }
+    if(activitySelect == 6)
+    {
+        activity = "Look for flowers!";
+    }
+    if(activitySelect == 7)
+    {
+        activity = "Learn a coding language!";
+    }
+    if(activitySelect == 8)
+    {
+        activity = "Read a book!";
+    }
+    if(activitySelect == 9)
+    {
+        activity = "Look for some spare change!";
+    }
+    if(activitySelect == 10)
+    {
+        activity = "Go swimming!";
+    }
+    if(activitySelect == 11)
+    {
+        activity = "Go take a nap!";
+    }
+    if(activitySelect == 12)
+    {
+        activity = "Watch TV!";
+    }
+    if(activitySelect == 13)
+    {
+        activity = "Talk to new people!";
+    }
+    if(activitySelect == 14)
+    {
+        activity = "Play with a friend!";
+    }
+    if(activitySelect == 15)
+    {
+        activity = "Tell scary ghost stories!";
+    }
+    if(activitySelect == 16)
+    {
+        activity = "Play chess!";
+    }
+    if(activitySelect == 17)
+    {
+        activity = "Take a break!";
+    }
+    if(activitySelect == 18)
+    {
+        activity = "Talk to someone!";
+    }
+    if(activitySelect == 19)
+    {
+        activity = "Make something!";
+    }
+    if(activitySelect == 20)
+    {
+        activity = "Draw something you like!";
+    }
+    if(activitySelect == 21)
+    {
+        activity = "Give a gift to someone!";
+    }
+    if(activitySelect == 22)
+    {
+        activity = "Donate money to charity!";
+    }
+    if(activitySelect == 23)
+    {
+        activity = "Cook a meal for someone!";
+    }
+    if(activitySelect == 24)
+    {
+        activity = "Make a sticker!";
+    }
+    if(activitySelect == 25)
+    {
+        activity = "Do origami!";
+    }
+    if(activitySelect == 26)
+    {
+        activity = "Buy a Raspberry Pi!";
+    }
+    if(activitySelect == 27)
+    {
+        activity = "Sell old stuff!";
+    }
+    if(activitySelect == 28)
+    {
+        activity = "Count your money!";
+    }
+    if(activitySelect == 29)
+    {
+        activity = "Feed some ducks!";
+    }
+    if(activitySelect == 30)
+    {
+        activity = "Build a robot!";
+    }
+    if(activitySelect == 31)
+    {
+        activity = "Drink water!";
+    }
+    if(activitySelect == 32)
+    {
+        activity = "Play some games!";
+    }
+    if(activitySelect == 33)
+    {
+        activity = "Look for a cat!";
+    }
+    if(activitySelect == 34)
+    {
+        activity = "Look for a dog!";
+    }
+    if(activitySelect == 35)
+    {
+        activity = "Do some homework!";
+    }
+    if(activitySelect == 36)
+    {
+        activity = "Have some fun!";
+    }
+    if(activitySelect == 37)
+    {
+        activity = "Join a organization!";
+    }
+    if(activitySelect == 38)
+    {
+        activity = "Start a club!";
+    }
+    if(activitySelect == 39)
+    {
+        activity = "Make a map!";
+    }
+    if(activitySelect == 40)
+    {
+        activity = "Make a project with code!";
+    }
+    if(activitySelect == 41)
+    {
+        activity = "Spend time with others!";
+    }
+    if(activitySelect == 42)
+    {
+        activity = "Bake cookies!";
+    }
+    if(activitySelect == 43)
+    {
+        activity = "Look for treasure!";
+    }
+    if(activitySelect == 44)
+    {
+        activity = "Make a time capsule!";
+    }
+    if(activitySelect == 45)
+    {
+        activity = "Make a sticky note wall!";
+    }
+    if(activitySelect == 46)
+    {
+        activity = "Sing a song!";
+    }
+    if(activitySelect == 47)
+    {
+        activity = "Mail something to someone!";
+    }
+    if(activitySelect == 48)
+    {
+        activity = "Go outside and take a walk!";
+    }
+    if(activitySelect == 49)
+    {
+        activity = "Make a video!";
+    }
+    if(activitySelect == 50)
+    {
+        activity = "Spread some love around!";
+    }
+    if(activitySelect == 51)
+    {
+        activity = "Crack some jokes!";
+    }
+    if(activitySelect == 52)
+    {
+        activity = "Create a new language!";
+    }
+    if(activitySelect == 53)
+    {
+        activity = "Create a new operating system!";
+    }
+    if(activitySelect == 54)
+    {
+        activity = "Find a solution to any problem!";
+    }
+    if(activitySelect == 55)
+    {
+        activity = "Make a wish!";
+    }
+    if(activitySelect == 56)
+    {
+        activity = "Code with friends!";
+    }
+    if(activitySelect == 57)
+    {
+        activity = "Work on a project with someone!";
+    }
+    if(activitySelect == 58)
+    {
+        activity = "Pet an animal!";
+    }
+    if(activitySelect == 59)
+    {
+        activity = "Find a zebra!";
+    }
+    if(activitySelect == 60)
+    {
+        activity = "Look for a Giraffe";
+    }
+    if(activitySelect == 61)
+    {
+        activity = "Eat some pie!";
+    }
+    if(activitySelect == 62)
+    {
+        activity = "Eat some cake!";
+    }
+    if(activitySelect == 63)
+    {
+        activity = "Find some gold!";
+    }
+    if(activitySelect == 64)
+    {
+        activity = "Code until you can't!";
+    }
+    if(activitySelect == 65)
+    {
+        activity = "Make music!";
+    }
+    if(activitySelect == 66)
+    {
+        activity = "Create something funny!";
+    }
+    if(activitySelect == 67)
+    {
+        activity = "Find a pool noodle!";
+    }
+    if(activitySelect == 68)
+    {
+        activity = "Look for a mop and clean!";
+    }
+    if(activitySelect == 69)
+    {
+        activity = "Make a box fort!";
+    }
+    if(activitySelect == 70)
+    {
+        activity = "Look for discounted stuff!";
+    }
+    if(activitySelect == 71)
+    {
+        activity = "";
     }
 
     document.getElementById("activity-output").innerHTML = activity;

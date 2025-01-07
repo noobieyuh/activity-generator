ddd09bb53307a37aa830ea5e3bc75123504d6c17
ToolSchism <88911522+ToolSchism@users.noreply.github.com>
2025-01-07 18:23:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,412 +1,113 @@
-function activityGenerator()
-{
-    var activity = "";
-    // generate 100 responses, (maybe 200 in the future?)
-    var activitySelect = (Math.floor(Math.random() * 100) + 1);
-    // for debug
-    console.log(activitySelect);
+// this is just my idea !! you don't need to take it or anything :)
+// this project is cool by the way
+function activityGenerator() {
+    const activities = [
+        "Go outside and take pictures!",
+        "Go look for geocaches!",
+        "Count how many trees you see outside!",
+        "Listen to new music genres!",
+        "Learn a new language!",
+        "Look for flowers!",
+        "Learn a coding language!",
+        "Read a book!",
+        "Look for some spare change!",
+        "Go swimming!",
+        "Go take a nap!",
+        "Watch TV!",
+        "Talk to new people!",
+        "Play with a friend!",
+        "Tell scary ghost stories!",
+        "Play chess!",
+        "Take a break!",
+        "Talk to someone!",
+        "Make something!",
+        "Draw something you like!",
+        "Give a gift to someone!",
+        "Donate money to charity!",
+        "Cook a meal for someone!",
+        "Make a sticker!",
+        "Do origami!",
+        "Buy a Raspberry Pi!",
+        "Sell old stuff!",
+        "Count your money!",
+        "Feed some ducks!",
+        "Build a robot!",
+        "Drink water!",
+        "Play some games!",
+        "Look for a cat!",
+        "Look for a dog!",
+        "Do some homework!",
+        "Have some fun!",
+        "Join a organization!",
+        "Start a club!",
+        "Make a map!",
+        "Make a project with code!",
+        "Spend time with others!",
+        "Bake cookies!",
+        "Look for treasure!",
+        "Make a time capsule!",
+        "Make a sticky note wall!",
+        "Sing a song!",
+        "Mail something to someone!",
+        "Go outside and take a walk!",
+        "Make a video!",
+        "Spread some love around!",
+        "Crack some jokes!",
+        "Create a new language!",
+        "Create a new operating system!",
+        "Find a solution to any problem!",
+        "Make a wish!",
+        "Code with friends!",
+        "Work on a project with someone!",
+        "Pet an animal!",
+        "Find a zebra!",
+        "Look for a Giraffe",
+        "Eat some pie!",
+        "Eat some cake!",
+        "Find some gold!",
+        "Code until you can't!",
+        "Make music!",
+        "Create something funny!",
+        "Find a pool noodle!",
+        "Look for a mop and clean!",
+        "Make a box fort!",
+        "Look for discounted stuff!",
+        "Debug some code!",
+        "Mess around with AI!",
+        "Make ASCII Art!",
+        "Build a robot!",
+        "Give away old stuff!",
+        "Take pictures!",
+        "Pick up trash outside!",
+        "Go to the animal shelter and take care of one!",
+        "Learn morse code!",
+        "Learn binary!",
+        "Learn about routers!",
+        "Learn Rust!",
+        "Create a pumpkin!",
+        "Check your emails!",
+        "Send a nice letter to someone!",
+        "Help someone!",
+        "Do a personality test!",
+        "Chase a butterfly!",
+        "Give someone a hug!",
+        "Check out Advent of Code!",
+        "Get a degree in something!",
+        "Make some funny clips!",
+        "Look at social media!",
+        "Learn about bash!",
+        "Learn command prompt!",
+        "Learn about Linux!",
+        "Learn about NixOs!",
+        "Learn more about Windows!",
+        "Learn more about pc parts!",
+        "Learn more about Cybersecurity!"
+    ];
 
-    if(activitySelect == 1)
-    {
-        activity = "Go outside and take pictures!";
-    }
-    if(activitySelect == 2)
-    {
-        activity = "Go look for geocaches!";
-    }
-    if(activitySelect == 3)
-    {
-        activity = "Count how many trees you see outside!";
-    }
-    if(activitySelect == 4)
-    {
-        activity = "Listen to new music genres!";
-    }
-    if(activitySelect == 5)
-    {
-        activity = "Learn a new language!";
-    }
-    if(activitySelect == 6)
-    {
-        activity = "Look for flowers!";
-    }
-    if(activitySelect == 7)
-    {
-        activity = "Learn a coding language!";
-    }
-    if(activitySelect == 8)
-    {
-        activity = "Read a book!";
-    }
-    if(activitySelect == 9)
-    {
-        activity = "Look for some spare change!";
-    }
-    if(activitySelect == 10)
-    {
-        activity = "Go swimming!";
-    }
-    if(activitySelect == 11)
-    {
-        activity = "Go take a nap!";
-    }
-    if(activitySelect == 12)
-    {
-        activity = "Watch TV!";
-    }
-    if(activitySelect == 13)
-    {
-        activity = "Talk to new people!";
-    }
-    if(activitySelect == 14)
-    {
-        activity = "Play with a friend!";
-    }
-    if(activitySelect == 15)
-    {
-        activity = "Tell scary ghost stories!";
-    }
-    if(activitySelect == 16)
-    {
-        activity = "Play chess!";
-    }
-    if(activitySelect == 17)
-    {
-        activity = "Take a break!";
-    }
-    if(activitySelect == 18)
-    {
-        activity = "Talk to someone!";
-    }
-    if(activitySelect == 19)
-    {
-        activity = "Make something!";
-    }
-    if(activitySelect == 20)
-    {
-        activity = "Draw something you like!";
-    }
-    if(activitySelect == 21)
-    {
-        activity = "Give a gift to someone!";
-    }
-    if(activitySelect == 22)
-    {
-        activity = "Donate money to charity!";
-    }
-    if(activitySelect == 23)
-    {
-        activity = "Cook a meal for someone!";
-    }
-    if(activitySelect == 24)
-    {
-        activity = "Make a sticker!";
-    }
-    if(activitySelect == 25)
-    {
-        activity = "Do origami!";
-    }
-    if(activitySelect == 26)
-    {
-        activity = "Buy a Raspberry Pi!";
-    }
-    if(activitySelect == 27)
-    {
-        activity = "Sell old stuff!";
-    }
-    if(activitySelect == 28)
-    {
-        activity = "Count your money!";
-    }
-    if(activitySelect == 29)
-    {
-        activity = "Feed some ducks!";
-    }
-    if(activitySelect == 30)
-    {
-        activity = "Build a robot!";
-    }
-    if(activitySelect == 31)
-    {
-        activity = "Drink water!";
-    }
-    if(activitySelect == 32)
-    {
-        activity = "Play some games!";
-    }
-    if(activitySelect == 33)
-    {
-        activity = "Look for a cat!";
-    }
-    if(activitySelect == 34)
-    {
-        activity = "Look for a dog!";
-    }
-    if(activitySelect == 35)
-    {
-        activity = "Do some homework!";
-    }
-    if(activitySelect == 36)
-    {
-        activity = "Have some fun!";
-    }
-    if(activitySelect == 37)
-    {
-        activity = "Join a organization!";
-    }
-    if(activitySelect == 38)
-    {
-        activity = "Start a club!";
-    }
-    if(activitySelect == 39)
-    {
-        activity = "Make a map!";
-    }
-    if(activitySelect == 40)
-    {
-        activity = "Make a project with code!";
-    }
-    if(activitySelect == 41)
-    {
-        activity = "Spend time with others!";
-    }
-    if(activitySelect == 42)
-    {
-        activity = "Bake cookies!";
-    }
-    if(activitySelect == 43)
-    {
-        activity = "Look for treasure!";
-    }
-    if(activitySelect == 44)
-    {
-        activity = "Make a time capsule!";
-    }
-    if(activitySelect == 45)
-    {
-        activity = "Make a sticky note wall!";
-    }
-    if(activitySelect == 46)
-    {
-        activity = "Sing a song!";
-    }
-    if(activitySelect == 47)
-    {
-        activity = "Mail something to someone!";
-    }
-    if(activitySelect == 48)
-    {
-        activity = "Go outside and take a walk!";
-    }
-    if(activitySelect == 49)
-    {
-        activity = "Make a video!";
-    }
-    if(activitySelect == 50)
-    {
-        activity = "Spread some love around!";
-    }
-    if(activitySelect == 51)
-    {
-        activity = "Crack some jokes!";
-    }
-    if(activitySelect == 52)
-    {
-        activity = "Create a new language!";
-    }
-    if(activitySelect == 53)
-    {
-        activity = "Create a new operating system!";
-    }
-    if(activitySelect == 54)
-    {
-        activity = "Find a solution to any problem!";
-    }
-    if(activitySelect == 55)
-    {
-        activity = "Make a wish!";
-    }
-    if(activitySelect == 56)
-    {
-        activity = "Code with friends!";
-    }
-    if(activitySelect == 57)
-    {
-        activity = "Work on a project with someone!";
-    }
-    if(activitySelect == 58)
-    {
-        activity = "Pet an animal!";
-    }
-    if(activitySelect == 59)
-    {
-        activity = "Find a zebra!";
-    }
-    if(activitySelect == 60)
-    {
-        activity = "Look for a Giraffe";
-    }
-    if(activitySelect == 61)
-    {
-        activity = "Eat some pie!";
-    }
-    if(activitySelect == 62)
-    {
-        activity = "Eat some cake!";
-    }
-    if(activitySelect == 63)
-    {
-        activity = "Find some gold!";
-    }
-    if(activitySelect == 64)
-    {
-        activity = "Code until you can't!";
-    }
-    if(activitySelect == 65)
-    {
-        activity = "Make music!";
-    }
-    if(activitySelect == 66)
-    {
-        activity = "Create something funny!";
-    }
-    if(activitySelect == 67)
-    {
-        activity = "Find a pool noodle!";
-    }
-    if(activitySelect == 68)
-    {
-        activity = "Look for a mop and clean!";
-    }
-    if(activitySelect == 69)
-    {
-        activity = "Make a box fort!";
-    }
-    if(activitySelect == 70)
-    {
-        activity = "Look for discounted stuff!";
-    }
-    if(activitySelect == 71)
-    {
-        activity = "Debug some code!";
-    }
-    if(activitySelect == 72)
-    {
-        activity = "Mess around with AI!";
-    }
-    if(activitySelect == 73)
-    {
-        activity = "Make ASCII Art!";
-    }
-    if(activitySelect == 74)
-    {
-        activity = "Build a robot!";
-    }
-    if(activitySelect == 75)
-    {
-        activity = "Give away old stuff!";
-    }
-    if(activitySelect == 76)
-    {
-        activity = "Take pictures!";
-    }
-    if(activitySelect == 77)
-    {
-        activity = "Pick up trash outside!";
-    }
-    if(activitySelect == 78)
-    {
-        activity = "Go to the animal shelter and take care of one!";
-    }
-    if(activitySelect == 79)
-    {
-        activity = "Learn morse code!";
-    }
-    if(activitySelect == 80)
-    {
-        activity = "Learn binary!";
-    }
-    if(activitySelect == 81)
-    {
-        activity = "Learn about routers!";
-    }
-    if(activitySelect == 82)
-    {
-        activity = "Learn Rust!";
-    }
-    if(activitySelect == 83)
-    {
-        activity = "Create a pumpkin!";
-    }
-    if(activitySelect == 84)
-    {
-        activity = "Check your emails!";
-    }
-    if(activitySelect == 85)
-    {
-        activity = "Send a nice letter to someone!";
-    }
-    if(activitySelect == 86)
-    {
-        activity = "Help someone!";
-    }
-    if(activitySelect == 87)
-    {
-        activity = "Do a personality test!";
-    }
-    if(activitySelect == 88)
-    {
-        activity = "Chase a butterfly!";
-    }
-    if(activitySelect == 89)
-    {
-        activity = "Give someone a hug!";
-    }
-    if(activitySelect == 90)
-    {
-        activity = "Check out Advent of Code!";
-    }
-    if(activitySelect == 91)
-    {
-        activity = "Get a degree in something!";
-    }
-    if(activitySelect == 92)
-    {
-        activity = "Make some funny clips!";
-    }
-    if(activitySelect == 93)
-    {
-        activity = "Look at social media!";
-    }
-    if(activitySelect == 94)
-    {
-        activity = "Learn about bash!";
-    }
-    if(activitySelect == 95)
-    {
-        activity = "Learn command prompt!";
-    }
-    if(activitySelect == 96)
-    {
-        activity = "Learn about Linux!";
-    }
-    if(activitySelect == 97)
-    {
-        activity = "Learn about NixOs!";
-    }
-    if(activitySelect == 98)
-    {
-        activity = "Learn more about Windows!";
-    }
-    if(activitySelect == 99)
-    {
-        activity = "Learn more about pc parts!";
-    }
-    if(activitySelect == 100)
-    {
-        activity = "Learn more about Cybersecurity!";
-    }
+    // Get a random activity from the array
+    const randomIndex = Math.floor(Math.random() * activities.length);
+    const activity = activities[randomIndex];
 
-
+    // Update the DOM
     document.getElementById("activity-output").innerHTML = activity;
 }
